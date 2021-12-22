290332c1bacfd7bd0db10a7c4e545b5181ec9df8
edgarbp95 <edgarbp95@hotmail.com>
2021-12-22 19:32:36 -0300

    act

--- article-preview-component-master/app.js
+++ article-preview-component-master/app.js
@@ -1,17 +1,25 @@
-let mobileActive = document.getElementById("mobile_active");
+let estado = false;
 
 // function myFunction() {
 
 document.getElementById('icon_active').addEventListener('click', function(){
-    let desktopActive = document.getElementById("desktop_active");
-    if(desktopActive.style.display==="none" && screen.width>="850"){
+    let desktopActive = document.querySelector(".desktop_share");
+    let mobileActive = document.querySelector(".mobile_share");
+    if(estado==false && screen.width>="850"){
         desktopActive.style.display="flex";
-    }else if (mobileActive.style.display==="none" && screen.width<"850"){
+        estado=true;
+    }else if (estado==false && screen.width<"850"){
         mobileActive.style.display="flex";
+        estado=true;
+            document.querySelector('.mobile_share_icon').addEventListener('click', function(){
+              mobileActive.style.display = "none";  
+              estado=false;
+            })
     }
      else {
         desktopActive.style.display = "none";
         mobileActive.style.display = "none";
+        estado=false;
     }
 })
 
